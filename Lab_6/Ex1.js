b73d153a15033a6f1ea15bb12655026022dbe81b
dolanq <89557159+dolanq@users.noreply.github.com>
2021-09-27 15:37:40 -1000

    wod

---
+++ Lab_6/Ex1.js
@@ -0,0 +1,11 @@
+ var age =  22;
+
+ var fav_num = 76;
+
+ var day_of_birth = 4;
+
+ var month_of_birth = 5;
+
+ console.log(age + fav_num / day_of_birth* month_of_birth);
+ 
+console.log( (age + fav_num) / day_of_birth * month_of_birth);
